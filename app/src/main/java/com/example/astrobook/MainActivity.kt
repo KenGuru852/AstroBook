071be28055770f8bea46808f152a4c5cd0e70988
alexey_pritula <pritula.alex91@mail.ru>
2024-09-19 12:07:42 +0700

    News are switchable

--- app/src/main/java/com/example/astrobook/MainActivity.kt
+++ app/src/main/java/com/example/astrobook/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.astrobook
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -19,6 +21,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
@@ -27,6 +32,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.astrobook.ui.theme.AstroBookTheme
+import kotlin.random.Random
 
 
 class MainActivity : ComponentActivity() {
@@ -40,37 +46,41 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DrawNews()
+                    AllNews()
                 }
             }
         }
     }
 }
 
+data class NewsItem(val imageResId: Int, val text: String, var likes: Int)
+
+
+
 @Composable
-fun DrawNews() {
+fun DrawNews(newsList: List<NewsItem>) {
     Column(modifier = Modifier.fillMaxSize()) {
         Row(modifier = Modifier.weight(1f)) {
             NewsScreen(
                 modifier = Modifier.weight(1f),
-                image = painterResource(id = R.drawable.image1),
+                image = painterResource(id = newsList[0].imageResId),
                 text = "Text 1"
             )
             NewsScreen(
                 modifier = Modifier.weight(1f),
-                image = painterResource(id = R.drawable.image2),
+                image = painterResource(id = newsList[1].imageResId),
                 text = "Text 2"
             )
         }
         Row(modifier = Modifier.weight(1f)) {
             NewsScreen(
                 modifier = Modifier.weight(1f),
-                image = painterResource(id = R.drawable.image3),
+                image = painterResource(id = newsList[2].imageResId),
                 text = "Text 3"
             )
             NewsScreen(
                 modifier = Modifier.weight(1f),
-                image = painterResource(id = R.drawable.image4),
+                image = painterResource(id = newsList[3].imageResId),
                 text = "Text 4"
             )
         }
@@ -98,7 +108,42 @@ fun NewsScreen(image: Painter, text: String, modifier: Modifier) {
         }
     }
 }
+@Composable
+fun AllNews() {
+    val newsList = remember {
+        mutableStateListOf(
+            NewsItem(R.drawable.image1, "LIKES: ", 0),
+            NewsItem(R.drawable.image2, "LIKES: ", 0),
+            NewsItem(R.drawable.image3, "LIKES: ", 0),
+            NewsItem(R.drawable.image4, "LIKES: ", 0),
+            NewsItem(R.drawable.image5, "LIKES: ", 0),
+            NewsItem(R.drawable.image6, "LIKES: ", 0),
+            NewsItem(R.drawable.image7, "LIKES: ", 0),
+            NewsItem(R.drawable.image8, "LIKES: ", 0),
+            NewsItem(R.drawable.image9, "LIKES: ", 0),
+            NewsItem(R.drawable.image10, "LIKES: ", 0)
+        )
+    }
+    val handler = Handler(Looper.getMainLooper())
+    val runnable = remember {
+        object : Runnable {
+            override fun run() {
+                val randomIndex = Random.nextInt(0, 4)
+                val randomNewsIndex = Random.nextInt(4, newsList.size)
+                val temp = newsList[randomIndex]
+                newsList[randomIndex] = newsList[randomNewsIndex]
+                newsList[randomNewsIndex] = temp
+                handler.postDelayed(this, 5000)
+            }
+        }
+    }
 
+    LaunchedEffect(Unit) {
+        handler.post(runnable)
+    }
+
+    DrawNews(newsList)
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
